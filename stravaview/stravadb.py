d60a299b58340d7bd06184bf61eb37b127cbeae8
Jerome Lelong <jerome.lelong@gmail.com>
2016-12-15 17:41:06 +0100

    Add an activity to the local table
    We also fix some column definitions in the table

--- stravaview/stravadb.py
+++ stravaview/stravadb.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 
 import stravalib.client
+import stravalib.unithelper
 import pymysql.cursors
+import pymysql.converters
 
 
 class Strava:
@@ -22,7 +24,7 @@ class Strava:
 
     def update_bikes(self):
         """
-        Update the database of bikes
+        Update the bikes table
         """
         # Connect to the database
         table = self.config['mysql_bikes_table']
@@ -39,6 +41,43 @@ class Strava:
             sql = "insert into %s (id, name, type, frame_type) values ('%s','%s', '%s', '%d')" % (table, desc.id, desc.name, self.FRAME_TYPES[desc.frame_type], desc.frame_type)
             self.cursor.execute(sql)
             self.connection.commit()
+
+    def push_activity(self, activity):
+        """
+        Add the activity `activity` to the activities table
+
+        activity: an object of class:`stravalib.model.Activity`
+        """
+        table = self.config['mysql_activities_table']
+        # Check if activity is already in the table
+        sql = "select * from %s where id='%s' limit 1" % (table, activity.id)
+        if (self.cursor.execute(sql) > 0):
+            print("Activity '%s' already exists in table" % (activity.name))
+            return
+
+        if (activity.type != u'Ride' or activity.commute):
+            print("Activity '%s' is not a pleasure ride" % (activity.name))
+            return
+        name = pymysql.converters.escape_string(activity.name)
+        distance = "%0.2f" % stravalib.unithelper.kilometers(activity.distance).get_num()
+        elevation = "%0.0f" % stravalib.unithelper.meters(activity.total_elevation_gain).get_num()
+        date = activity.start_date_local
+        location = activity.location_city
+        moving_time = activity.moving_time
+        elapsed_time = activity.elapsed_time
+        gear_id = activity.gear_id
+        average_speed = "%0.1f" % stravalib.unithelper.kilometers_per_hour(activity.average_speed).get_num()
+        average_heartrate = "%0.0f" % activity.average_heartrate
+        max_heartrate = activity.max_heartrate
+        suffer_score = activity.suffer_score
+
+        sql = """insert into %s (id, name, distance, elevation, date, location, moving_time, elapsed_time,
+        gear_id, average_speed, average_heartrate, max_heartrate, suffer_score)
+        values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
+        """ % (table, activity.id, name, distance, elevation, date, location, moving_time, elapsed_time,
+               gear_id, average_speed, average_heartrate, max_heartrate, suffer_score)
+        self.cursor.execute(sql)
+        self.connection.commit()
 
     def create_bikes_table(self):
         """
@@ -81,12 +120,12 @@ class Strava:
         distance float DEFAULT 0,
         elevation float DEFAULT 0,
         moving_time time DEFAULT NULL,
-        ellapsed_time time DEFAULT NULL,
-        gear_id varchar(45) DEFAuLT NULL,
+        elapsed_time time DEFAULT NULL,
+        gear_id varchar(45) DEFAULT NULL,
         average_speed float DEFAULT NULL,
-        max_heartrate int DEFAULT 0,
-        average_heartrate float DEFAULT 0,
-        suffer_score int DEFAULT 0,
+        max_heartrate int DEFAULT NULL,
+        average_heartrate float DEFAULT NULL,
+        suffer_score int DEFAULT NULL,
         PRIMARY KEY (id),
         UNIQUE KEY strid_UNIQUE (id)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8""" % table
