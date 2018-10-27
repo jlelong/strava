e876a0e2c911ed75c06d3463bd24ef9a175fcb47
Jerome Lelong <jerome.lelong@gmail.com>
2018-10-27 10:13:49 +0200

    Fix filter
    To apply the filter to several fields, we concatenate them. Make sure to
    insert a space between them to avoid false matches.

--- wui/app/app.js
+++ wui/app/app.js
@@ -335,7 +335,13 @@ function StravaController($cookies, $scope, $window, $http, $timeout) {
         return function (obj) {
             if (!regex)
                 return true;
-            return (removeAccents(obj.name + obj.location + obj.date + obj.gear + obj.description).match(regex) !== null);
+            // const allFields = removeAccents([obj.name, obj.location, obj.date, obj.gear, obj.description].join(' '));
+            // const isMatchted = allFields.match(regex)
+            // if (isMatchted) {
+            //     console.log(allFields)
+            // }
+            // return isMatchted != null
+            return (removeAccents([obj.name, obj.location, obj.date, obj.gear, obj.description].join(' ')).match(regex) !== null);
         };
     }
 
