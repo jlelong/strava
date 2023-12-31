c16e15bb892a38d35ad586e1a31d86357f68476c
Jerome Lelong <jerome.lelong@univ-grenoble-alpes.fr>
2023-12-31 06:58:40 +0100

    Use geopy for reverse GPS location
    The package geocoder does not seem maintained anymore and a change in OSM API breaks it.

--- backend/utils.py
+++ backend/utils.py
@@ -1,4 +1,5 @@
-import geocoder
+from geopy.geocoders import Nominatim
+from geopy.exc import GeopyError
 
 def get_location(cords):
     """
@@ -7,30 +8,29 @@ def get_location(cords):
 
     :param cords: a pair of (latitude, longitude) coordinates
     :type cords: a list or a tuple
-
-    :param geolocator: an instance of a geocoder capable of reverse locating
     """
     if cords is None:
         return None
     max_attempts = 4
     attempts = 0
+    geolocator = Nominatim(user_agent="StravaView")
     while True:
         try:
-            # location = geolocator.reverse(cords)
-            location = geocoder.osm(f"{cords.lat},{cords.lon}.")
-            if location.json is None:
+            location = geolocator.reverse(cords)
+            if location.raw is None:
                 return None
-            location_dict = location.json
+            location_dict = location.raw['address']
             city = ""
             code = ""
-            for key in ('hamlet', 'village', 'city', 'town', 'state'):
+            for key in ('village', 'hamlet', 'city', 'town', 'state'):
                 if key in location_dict:
                     city = location_dict[key]
                     break
-            if location_dict['country'] == 'France' and 'postal' in location_dict:
-                code = ' (' + location_dict['postal'][0:2] + ')'
+            if location_dict['country'] == 'France' and 'postcode' in location_dict:
+                code = ' (' + location_dict['postcode'][0:2] + ')'
             return city + code
-        except:
+        except GeopyError as e:
             attempts += 1
             if attempts > max_attempts:
+                print(f"Error getting reverse location for {cords.lat},{cords.lon}", e)
                 break
